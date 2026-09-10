d081189c8968de1c59c0972e6879e0302c910d28
Jordan Deklerk <111652310+jordandeklerk@users.noreply.github.com>
2026-09-10 19:40:28 -0400

    Move channel selection into the data module

--- mmmjax/__init__.py
+++ mmmjax/__init__.py
@@ -1,8 +1,7 @@
 """Bayesian marketing mix modeling expressed entirely in JAX."""
 
 from mmmjax.adstock import delayed_adstock, geometric_adstock, weibull_cdf_adstock, weibull_pdf_adstock
-from mmmjax.channels import select_channels
-from mmmjax.data import PreparedData, prepare_data
+from mmmjax.data import PreparedData, prepare_data, select_channels
 from mmmjax.distributions import (
     bernoulli,
     bernoulli_logcdf,
